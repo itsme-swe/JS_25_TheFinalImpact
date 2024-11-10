bbdd49801e0c9e0b12a5739fcc9f760f36319255
itsme-swe <me.harshmehra07@gmail.com>
2024-11-10 08:16:08 +0530

    update

---
+++ 03_DataTypeConversion/1_ConversionOperation.js
@@ -0,0 +1,24 @@
+/*
+💥 Converting data type into another data type.
+
+*/
+
+let age = "32";
+
+console.log(typeof age); // Output: string
+
+// Here we are converting string type into number type
+let ageInNum = Number(age);
+
+console.log(typeof ageInNum); // Output: number
+
+console.log();
+
+let dob = "5june2025";
+
+console.log(typeof dob);
+
+let dobInNum = Number(dob);
+
+console.log(typeof dobInNum); // Output: number
+console.log(dobInNum); // Output: Nan
---
+++ 03_DataTypeConversion/2_operations.js
@@ -0,0 +1,13 @@
+let value = 3;
+
+let negVal = -value;
+
+console.log(negVal); // Output: -3
+
+console.log();
+
+let negValue = -20;
+
+let postVal = -negValue;
+
+console.log(postVal); // Output: 20
