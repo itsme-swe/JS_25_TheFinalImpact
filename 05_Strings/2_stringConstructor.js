e6904b63628cf9d8656b549c265bc0f908182d6d
itsme-swe <me.harshmehra07@gmail.com>
2024-11-10 13:07:27 +0530

    update

---
+++ 05_Strings/2_stringConstructor.js
@@ -0,0 +1,27 @@
+/*
+💥 Creating string using constructor
+
+*/
+
+let gameName = new String("Battle Field");
+
+console.log(gameName); // Output: [String: 'Battle Field']
+console.log(typeof gameName); // Output: Object
+
+console.log();
+
+/*
+🔸 Some most commonly used methods of String 
+1️⃣ .length
+2️⃣ .slice(startIndex, endIndex) : extracts a part of the string based on the given stating-index and ending-index and returns a new string.
+3️⃣
+4️⃣
+5️⃣6️⃣7️⃣8️⃣9️⃣🔟
+ */
+
+let a = 10;
+let b = 20;
+
+console.log(gameName.length); // Output: 12
+
+console.log(gameName.slice(1, 6)); // Output: attle
