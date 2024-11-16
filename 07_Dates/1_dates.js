ae3188a867df713c15579654758714e16bbc53d9
itsme-swe <me.harshmehra07@gmail.com>
2024-11-16 18:47:20 +0530

    update

---
+++ 07_Dates/1_dates.js
@@ -0,0 +1,13 @@
+// Creating object of Date
+let myDate = new Date();
+
+console.log(myDate); // o/p: 2024-11-16T11:10:49.018Z
+
+// We can convert date object into string
+console.log(myDate.toString()); // o/p: Sat Nov 16 2024 16:41:56 GMT+0530 (India Standard Time)
+
+console.log(myDate.toISOString()); // o/p: 2024-11-16T11:13:38.750Z
+
+console.log(myDate.toLocaleString()); // o/p: 11/16/2024, 4:43:38 PM
+
+console.log(myDate.toLocaleDateString()); // o/p: 11/16/2024
