256600d4da95234eac6853b35576ab637e7d1bdf
itsme-swe <me.harshmehra07@gmail.com>
2024-11-19 09:40:36 +0530

    update

--- 08_Arrays/4_arrayMethods.js
+++ 08_Arrays/4_arrayMethods.js
@@ -1,1 +1,13 @@
 let details = ["Harsh", 32, 70.5, true];
+
+let rollNo = [20, 30, 40, 50];
+
+console.log(details.includes(32)); // returns ture value if element is present inside an array otherwise false.
+
+console.log(details.indexOf(true)); // return the indexValue of an element
+
+console.log(details.indexOf(50)); // if element is not present returns -1.
+
+let newArr = rollNo.join(); // method is used to combine all elements of an array into a single string.
+
+console.log(newArr); // o/p:20,30,40,50
---
+++ 08_Arrays/5_slice_spliceMethod.js
@@ -0,0 +1,27 @@
+//💥 slice() and splice() Method
+
+let sedanCar = ["Virtus", "Slavia", "Verna", "Ciaz", "Altis", "Camry"];
+
+let companyName = ["Volkswagen", "Skoda", "Hyundai", "Maruti", "Toyota", "BMW"];
+
+//1️⃣ slice() method returns a new array containing a portion of the original array, based on the start and end index provided as arguments. But slice() extracts without modifying the original array.
+
+console.log("Before Sliced:", sedanCar); // o/p: Before Sliced: [ 'Virtus', 'Slavia', 'Verna', 'Ciaz', 'Altis', 'Camry' ]
+
+let someCars = sedanCar.slice(1, 4);
+
+console.log("After Sliced:", someCars); // o/p: After Sliced: [ 'Slavia', 'Verna', 'Ciaz' ]
+
+console.log("Original Array:", sedanCar); // o/p: Original Array: [ 'Virtus', 'Slavia', 'Verna', 'Ciaz', 'Altis', 'Camry' ]
+
+console.log();
+
+// 2️⃣ spliced() method is used to add, remove, or replace elements in an array. And returns an returns an array containing the elements that were removed.
+
+console.log("Before Spliced: ", companyName); // o/p: Before Spliced:  [ 'Volkswagen', 'Skoda', 'Hyundai', 'Maruti', 'Toyota', 'BMW' ]
+
+let carCompany = companyName.splice(1, 4);
+
+console.log("After spliced:", carCompany); // o/p: After spliced: [ 'Skoda', 'Hyundai', 'Maruti', 'Toyota' ]
+
+console.log("Original Array: ", companyName); // o/p: Original Array:  [ 'Volkswagen', 'BMW' ]
