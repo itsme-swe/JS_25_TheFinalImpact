c47bf4ac6759fd79570bb5f1ab53348e5d796214
itsme-swe <me.harshmehra07@gmail.com>
2024-11-19 20:38:21 +0530

    up

--- 08_Arrays/7_isArray_from_ofMethod.js
+++ 08_Arrays/7_isArray_from_ofMethod.js
@@ -7,3 +7,11 @@ console.log(Array.isArray("Skoda")); // return false bcoz skoda is string
 console.log();
 
 console.log(Array.from("Harsh")); // o/p: [ 'H', 'a', 'r', 's', 'h' ] from() method convert array to string
+
+console.log();
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3)); // o/p: [ 100, 200, 300 ] convert all elements into array.
