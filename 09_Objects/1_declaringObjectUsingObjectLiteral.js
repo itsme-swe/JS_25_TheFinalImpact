68f2a921d8646f35de15d574a879e52618eed25f
itsme-swe <me.harshmehra07@gmail.com>
2024-11-20 11:13:45 +0530

    update

---
+++ 09_Objects/1_declaringObjectUsingObjectLiteral.js
@@ -0,0 +1,15 @@
+// Creating Object using Object Literal
+
+const jsUser = {
+  name: "Harsh",
+  age: 32,
+  weight: 70.5,
+  location: "jaipur",
+};
+
+// 💥 Accessing objects in JS
+console.log(jsUser["age"]); // 1️⃣ using [ ] brackets
+
+console.log();
+
+console.log(jsUser.age); // 2️⃣ using dot'.'
---
+++ 09_Objects/2_lockingAnObject.js
@@ -0,0 +1,16 @@
+// We an freeze our objects in JS. If we freeze our object nobody can change our object properties.
+
+let newUser = {
+  name: "Goku",
+  email: "goku@gmail",
+  age: 26,
+  color: "yellow",
+};
+
+Object.freeze(newUser); // Locking or freezing our object
+
+newUser.name = "Moku";
+
+console.log(newUser);
+
+// o/p: { name: 'Goku', email: 'goku@gmail', age: 26, color: 'yellow' }
