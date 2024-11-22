6fc5b6e21a53bb25cf4d02312443427ffdb4c620
itsme-swe <me.harshmehra07@gmail.com>
2024-11-22 08:24:20 +0530

    update

--- 12_ScopingInJavaScript/main.js
+++ 12_ScopingInJavaScript/main.js
@@ -13,6 +13,7 @@ a();
 /*
 
 1️⃣ Lexical Environment ⇒ Lexical(Hierarchy or in-order) environment is the local memory along with the lexical environment of its parent.
+    Lexical Environment ka matlab hai ek function ka local memory aur uske parent ka reference. Jab koi function create hota hai, uska ek scope chain banta hai jo uske parent ke context se connected hota hai.
 
 🔸 So in the above example the lexical environment of function c() is function a() and the function a() lexical environment is global object and lexical environment of global object is pointing to null.
 
