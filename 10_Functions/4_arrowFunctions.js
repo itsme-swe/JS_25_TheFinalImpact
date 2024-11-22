52d9361b01c3345caa1e24d3e6e13b399041b0c6
itsme-swe <me.harshmehra07@gmail.com>
2024-11-23 05:21:15 +0530

    update

---
+++ 10_Functions/4_arrowFunctions.js
@@ -0,0 +1,15 @@
+//1️⃣ Basic Arrow Function
+const sum = (num1, num2) => {
+  return num1 + num2;
+};
+
+let total = sum(20, 20);
+
+console.log(total); // o/p: 40
+
+console.log();
+
+//2️⃣ Implicit return Arrow Function
+const addTwo = (num1, num2) => num1 + num2;
+
+console.log(addTwo(30, 20)); // o/p: 50
