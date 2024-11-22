f6c468d0d0271f5074711d8c7b11ed445463368d
itsme-swe <me.harshmehra07@gmail.com>
2024-11-22 05:43:12 +0530

    update

--- 10_Functions/2_restOperatorInFunctions.js
+++ 10_Functions/2_restOperatorInFunctions.js
@@ -8,6 +8,6 @@ console.log(totalCartValue); // o/p: [ 200, 200, 400, 500 ]
 
 /*
 
-Rest Operator: The rest operator (...) in JavaScript is used to gather the rest of the elements or properties into a new array or object. It's especially useful when working with functions, arrays, and objects.
+🔸 Rest Operator: The rest operator (...) in JavaScript is used to gather the rest of the elements or properties into a new array or object. It's especially useful when working with functions, arrays, and objects.
 
 */
---
+++ 11_Hoisting/1_hoisting.js
@@ -0,0 +1,13 @@
+console.log(sum(10, 10)); // o/p : 20
+
+console.log(a); // o/p : undefined
+
+// console.log(x);    // o/p: Throw an error
+
+var a = 20;
+
+let x = 10;
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
