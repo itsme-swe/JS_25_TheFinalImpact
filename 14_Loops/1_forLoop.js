113f9debc430758218ee751def844f01b9b2732d
itsme-swe <me.harshmehra07@gmail.com>
2024-11-24 19:09:13 +0530

    update

---
+++ 14_Loops/1_forLoop.js
@@ -0,0 +1,5 @@
+// Printing counting from 1 to 10 using for loop.
+
+for (let i = 1; i <= 10; i++) {
+  console.log(i + " ");
+}
---
+++ 14_Loops/2_whileLoop.js
@@ -0,0 +1,21 @@
+// Iterating over an array using while loop
+
+let cars = ["Slavia", "Virtus", "Amaze", "City", "Verna"];
+
+let i = 0; // Initilization Stage
+
+// Condition Stage
+while (i < cars.length) {
+  console.log(cars[i]);
+  i++; // Incrementing Stage
+}
+
+/*
+
+Slavia
+Virtus
+Amaze
+City
+Verna
+
+*/
