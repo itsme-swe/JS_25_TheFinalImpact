54cce1760b7fbbc3a9242e822e5fa11a67c9bb28
itsme-swe <me.harshmehra07@gmail.com>
2024-11-24 20:09:31 +0530

    update

--- 15_HigherOrderArrayLoops/2_forInLoop.js
+++ 15_HigherOrderArrayLoops/2_forInLoop.js
@@ -4,3 +4,16 @@ let myObj = {
   rb: "Ruby",
   go: "GoLang",
 };
+
+for (let key in myObj) {
+  console.log(`${key} is for ${myObj[key]}`);
+}
+
+/*
+
+js is for JavaScript
+cpp is for C++
+rb is for Ruby
+go is for GoLang
+
+*/
