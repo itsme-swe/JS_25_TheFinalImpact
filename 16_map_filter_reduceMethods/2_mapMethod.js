f7efff7a82ba5fc05de50bd54f36080dffff8613
itsme-swe <me.harshmehra07@gmail.com>
2024-11-25 19:32:05 +0530

    update

--- 16_map_filter_reduceMethods/2_mapMethod.js
+++ 16_map_filter_reduceMethods/2_mapMethod.js
@@ -5,3 +5,20 @@ let newNums = nums.map((val) => {
 });
 
 console.log(newNums); // o/p: [ 4, 8, 12, 16, 20 ]
+
+console.log();
+
+//🍁 Chaining in map method
+
+let total = nums
+  .map((val) => {
+    return val + 10;
+  })
+  .map((val) => {
+    return val * 2;
+  })
+  .filter((ele) => {
+    return ele > 30;
+  });
+
+console.log(total); // o/p: [ 32, 36, 40 ]
