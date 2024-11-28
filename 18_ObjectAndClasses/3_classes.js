5e06d653e967237a888db72b2b9fd33b1aa63ee9
itsme-swe <me.harshmehra07@gmail.com>
2024-11-28 08:27:09 +0530

    update

---
+++ 18_ObjectAndClasses/3_classes.js
@@ -0,0 +1,33 @@
+/*
+1️⃣ Class is a template for creating objects. Those objects will have some state(variables) and behaviour(functions) inside it.
+
+Syntax: 
+        class className {
+
+            constructor() {...}
+            methods() {....}
+
+        };
+
+*/
+
+//💥Creating Class
+class ToyotaCar {
+  constructor(brand, mileage) {
+    console.log("New Object Created");
+    this.brand = brand;
+    this.mileage = mileage;
+  }
+  start() {
+    console.log("Start");
+  }
+
+  stop() {
+    console.log("Stop");
+  }
+}
+
+//💥Now creating object from class template
+let camry = new ToyotaCar("Fortuner", 22);
+
+console.log(camry); // o/p: ToyotaCar { brand: 'Fortuner', mileage: 22 }
