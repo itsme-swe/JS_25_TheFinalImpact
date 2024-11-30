5b711024008f799305ea3f58b83e590b69192a90
itsme-swe <me.harshmehra07@gmail.com>
2024-11-30 08:51:53 +0530

    update

--- 18_ObjectAndClasses/3_classes.js
+++ 18_ObjectAndClasses/3_classes.js
@@ -32,3 +32,44 @@ class ToyotaCar {
 let camry = new ToyotaCar("Fortuner", 22);
 
 console.log(camry); // o/p: ToyotaCar { brand: 'Fortuner', mileage: 22 }
+
+console.log();
+
+//2️⃣ Here we are creating new Class
+
+class User {
+  constructor(username) {
+    this.username = username;
+  }
+
+  logMe() {
+    console.log(`username is ${this.username}`);
+  }
+}
+
+class Teacher extends User {
+  constructor(username, email, password) {
+    super(username); // calling parent class constructor with the help of 'super' keyword
+    this.email = email;
+    this.password = password;
+  }
+
+  addCourse() {
+    console.log(`A new course was added by ${this.username}`);
+  }
+}
+
+// 💥 Creating object t1 with the reference of Teacher Class
+const t1 = new Teacher("Mamata", "mam@school.com", "pap123");
+
+t1.logMe(); // o/p: username is Mamata
+
+t1.addCourse(); // o/p: A new course was added by Mamata
+
+console.log();
+
+//💥 Creating object t2 of User Class ⇒ Here object with the help of parent class cannot use properties or methods of child class.
+
+const t2 = new User("Sonal", "son@gmail.com", "son123");
+
+console.log(t2); // o/p: User { username: 'Sonal' }
