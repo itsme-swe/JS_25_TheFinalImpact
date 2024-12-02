45d53a7cdd37e1ce349e8bfd7d847d09982f4f10
itsme-swe <me.harshmehra07@gmail.com>
2024-12-02 13:55:01 +0530

    update

--- 25_PromiseAPIs/4_promiseAny.js
+++ 25_PromiseAPIs/4_promiseAny.js
@@ -10,10 +10,20 @@ const p3 = new Promise((res, rej) => {
 
 Promise.any([p1, p2, p3])
   .then((res) => console.log(res))
-  .catch((err) => console.error(err));
+  .catch((err) => {
+    console.error(err);
+    console.log(err.errors);
+  });
 
 /*
 Promise.any() will wait for the first setteled success promise. And, it will throw an "AggregateError" if all the promises setteled rejected.
 
 o/p: p3 success
+
+If all the promises setteled failed then output will look like:
+[AggregateError: All promises were rejected] {
+  [errors]: [ 'p1 success', 'p2 fail', 'p3 success' ]
+}
+[ 'p1 success', 'p2 fail', 'p3 success' ]
+ 
 */
