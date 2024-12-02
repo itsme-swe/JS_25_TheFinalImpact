dd4eed4ca03b496b679911883b5f5730178fb145
itsme-swe <me.harshmehra07@gmail.com>
2024-12-02 11:28:15 +0530

    update

---
+++ 25_PromiseAPIs/3_promiseRace.js
@@ -0,0 +1,15 @@
+const p1 = new Promise((res, rej) => {
+  setTimeout(() => res("p1 success"), 3000);
+});
+const p2 = new Promise((res, rej) => {
+  setTimeout(() => rej("p2 fail"), 1000);
+});
+const p3 = new Promise((res, rej) => {
+  setTimeout(() => res("p1 success"), 2000);
+});
+
+Promise.race([p1, p2, p3])
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err));
+
+// o/p: p2 fail ---- Promise.race() will return the first promise whether it is successfull or fail.
